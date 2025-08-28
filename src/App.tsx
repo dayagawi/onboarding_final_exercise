9579629a44d8b8b872c02f84404d22212b9713d9
Daya Matok Gawi <daya8694@gmail.com>
2025-08-28 15:56:08 +0300

    Initial empty commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+// import { useState } from 'react'
+import './App.css'
+import CreatorCard from './components/CreatorCard'
+
+function App() {
+  const handleDetailsClick = () => {
+    console.log('Details button clicked!');
+  };
+
+  return (
+    <>
+      <h1>User Profile Cards</h1>
+      <div style={{ display: 'flex', gap: '20px' }}>
+        <CreatorCard
+          name="Emily Johnson"
+          age={34}
+          location="Chicago IL"
+          email="email@x.dummyjson.com"
+          onDetailsClick={handleDetailsClick}
+        />
+        <CreatorCard
+          name="Emily Johnson"
+          age={34}
+          location="Chicago IL"
+          email="email@x.dummyjson.com"
+          onDetailsClick={handleDetailsClick}
+        />
+      </div>
+    </>
+  )
+}
+
+export default App
