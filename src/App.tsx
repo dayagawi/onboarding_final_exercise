3c0c621f4dfc7ae78be8cd2a508a6e2c4642b7af
dayagawi <daya8694@gmail.com>
2025-09-07 15:37:18 +0300

    Creator card branch (#4)
    CreatorCard: Add component

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,45 @@
-import { Avatar } from './components/Avatar'
+import { CreatorCard } from './components/CreatorCard'
+import styles from './App.module.css'
 
 function App() {
+  const handleDetailsClick = () => {
+    console.log('Details button clicked!');
+  };
 
   return (
-    <div style={{ display: 'flex', gap: '50px', alignItems: 'center' }}>
-      <Avatar 
-        type="icon"
-        size="sm"
-      />
-      <Avatar 
-        type="icon"
-        size="md"
-      />
-      <Avatar 
-        type="icon"
-        size="lg"
-      />
-      <Avatar 
-        type="icon"
-        size="xl"
-      />
-
-      <Avatar 
-        type="initials"
-        initials="AJ"
-        size="sm"
-      />
-      <Avatar 
-        type="initials"
-        initials="AJ"
-        size="md"
-      />
-      <Avatar 
-        type="initials"
-        initials="AJ"
-        size="lg"
-      />
-      <Avatar 
-        type="initials"
-        initials="BAjhdhY"
-        size="xl"
-      />
-    </div>
+    <>
+      <h1>User Profile Cards</h1>
+      <div className={styles['cards-container']}>
+        <CreatorCard
+          name="Emiliana Castillo Johnson Smith Kumar"
+          age={34}
+          location="Chicago IL"
+          email="emiliana.castillo@email.com"
+          onDetailsClick={handleDetailsClick}
+        />
+        <CreatorCard
+          name="Marryanna Johanson"
+          age={34}
+          location="Miami, FL"
+          email="email@x.dummyjson.com"
+          onDetailsClick={handleDetailsClick}
+        />
+        <CreatorCard
+          name="Yoisel Gonzalez"
+          age={67}
+          location="New York"
+          email="emailemailemailemail@x.dummyjson.com"
+          onDetailsClick={handleDetailsClick}
+        />
+        <CreatorCard
+          name="Benjamin Smith"
+          age={100}
+          location="very long location name that should be truncated at the end"
+          email="emailemailemailemail@x.dummyjson.com"
+          onDetailsClick={handleDetailsClick}
+        />
+      </div>
+    </>
   )
 }
 
